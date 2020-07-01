3fa80c3c1b4cacafcfe6563062538692b3911f44
cx-Zh <491907188@qq.com>
2020-07-02 00:00:24 +0800

    add if condition in Profile

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
 import Header from './layout/Header';
 import Menu from './layout/Menu';
 import Home from './views/Home';
 import { modules } from './utils';
 import './App.scss';
-// import {connect} from "react-redux";
 
 class App extends Component {
   render() {
@@ -19,11 +19,13 @@ class App extends Component {
               <Route exact path="/">
                 <Home />
               </Route>
-              {modules.map(({ path, component }) => (
-                <Route key={path} path={path}>
-                  {component}
-                </Route>
-              ))}
+              {this.props.sign === 'Sign out' && this.props.user.permissions
+                ? modules.map(({ path, component }) => (
+                    <Route key={path} path={path}>
+                      {component}
+                    </Route>
+                  ))
+                : null}
               <Route path="*">
                 <Home />
               </Route>
@@ -35,11 +37,11 @@ class App extends Component {
   }
 }
 
-// const mapStateToProps = ({ sign, user }) => ({
-//   sign,
-//   user
-// });
-//
-// export default connect(mapStateToProps)(App);
+const mapStateToProps = ({ sign, user }) => ({
+  sign,
+  user
+});
 
-export default App;
+export default connect(mapStateToProps)(App);
+
+// export default App;
--- src/views/Profile/index.js
+++ src/views/Profile/index.js
@@ -6,12 +6,21 @@ class Profile extends Component {
     return (
       <div className="main-view profile">
         <h2>个人信息</h2>
-        <ul>
-          <li>用户名：{this.props.user.name}</li>
-          <li>用户id：{this.props.user.id}</li>
-          <li>用户头像：</li>
-          <img src={this.props.user.avatar} alt="头像" />
-        </ul>
+        {this.props.sign === 'Sign out' ? (
+          <ul>
+            <li>用户名：{this.props.user.name}</li>
+            <li>用户id：{this.props.user.id}</li>
+            <li>用户头像：</li>
+            <img src={this.props.user.avatar} alt="头像" />
+          </ul>
+        ) : (
+          <ul>
+            <li>用户名：</li>
+            <li>用户id：</li>
+            <li>用户头像：</li>
+            <img src="" alt="头像" />
+          </ul>
+        )}
       </div>
     );
   }
